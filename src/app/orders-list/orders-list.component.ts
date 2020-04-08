ed6a6c4f0420b7cd2cbbd8718174624292258af1
abdulazizabdulnoor@gmail.com <abdulazizabdulnoor@gmail.com>
2020-04-08 19:22:23 +0800

    test

---
+++ src/app/orders-list/orders-list.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { OrdersService } from "../shared/orders.service";
+
+@Component({
+  selector: 'app-orders-list',
+  templateUrl: './orders-list.component.html',
+  styleUrls: ['./orders-list.component.scss']
+})
+export class OrdersListComponent implements OnInit {
+
+ constructor(private ordersService:OrdersService){}
+
+  ngOnInit() {this.getCoffeeOrders();}
+
+  coffeeOrders;
+
+  getCoffeeOrders = () => this.ordersService.getCoffeeOrders().subscribe(res =>(this.coffeeOrders = res));
+
+  markCompleted = data => this.ordersService.updateCoffeeOrder(data);
+
+  deleteOrder = data => this.ordersService.deleteCoffeeOrder(data);
+}
