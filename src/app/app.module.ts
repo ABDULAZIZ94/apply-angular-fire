96ac05fb07d27bbede93db1cbd97d1cc25c77bd7
ABDULAZIZ94 <abdulazizabdulnoor@gmail.com>
2020-04-21 08:35:18 +0800

    added icons

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,27 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { CommonModule } from '@angular/common';
 import { AppComponent } from './app.component';
 import { OrdersComponent } from './orders/orders.component';
 import { OrdersListComponent } from './orders-list/orders-list.component';
 
-import { ReactiveFormsModule } from "@angular/forms";
+import { ReactiveFormsModule, FormsModule } from "@angular/forms";
 
 import { environment } from "src/environments/environment";
 import { AngularFireModule } from "@angular/fire";
 import { AngularFirestoreModule, SETTINGS as FIRESTORE_SETTINGS } from '@angular/fire/firestore';
+import { SocialLoginComponent } from './social-login/social-login.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
   declarations: [
     AppComponent,
     OrdersComponent,
-    OrdersListComponent
+    OrdersListComponent,
+    SocialLoginComponent
   ],
   imports: [
     BrowserModule,
+    CommonModule,
+    FormsModule,
     ReactiveFormsModule,
     AngularFireModule.initializeApp(environment.firebaseConfig),
-    AngularFirestoreModule
+    AngularFirestoreModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/social-login/social-login.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { faGoogle, faTwitter, faGithub } from '@fortawesome/free-brands-svg-icons';
+import { faPhone } from '@fortawesome/free-solid-svg-icons';
+
+@Component({
+  selector: 'app-social-login',
+  templateUrl: './social-login.component.html',
+  styleUrls: ['./social-login.component.scss']
+})
+export class SocialLoginComponent implements OnInit {
+
+  id = new FormControl("");
+  pswd = new FormControl("");
+  faGithub = faGithub;
+  faTwitter = faTwitter;
+  faGoogle = faGoogle;
+  faPhone = faPhone;
+  
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
